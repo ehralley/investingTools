3bb94f29a000d3ed6e1df6917c9388e0fa05f7fc
Ted Alley <ehralley@TAF3E.WPA.Dal.Ca>
2016-09-13 10:54:18 -0300

    Can look up stock symbol from either Canadian and US market and get stock data now.

--- ROICscrape.py
+++ ROICscrape.py
@@ -1,15 +1,8 @@
 import mechanize
-from bs4 import BeautifulSoup as BS
+from bs4 import BeautifulSoup
 
-while 1:
-	print "Please Input Stock Symbol: "
-	stockSymbolInput = raw_input()
-	if stockSymbolInput.isalpha():
-		break
-	print "Symbol input not a string, please try again"
 
-stockCountry = 'US'
-stockSymbol = stockSymbolInput
+
 
 br = mechanize.Browser()
 
@@ -28,16 +21,37 @@ br.set_handle_refresh(False)
 br.addheaders = [('User-agent','Mac Safari')]
 
 
+while 1:
+	try:
+		print "Please Input Stock Symbol: "
+		stockSymbolInput = raw_input()
+		response = br.retrieve("http://www.google.com/finance/info?infotype=infoquoteall&q=" + stockSymbolInput, 'GoogleData.txt')
+	except:
+		print "Google can't find that stock symbol, try again"
+	else:
+		break
 
-response = br.get("http://www.google.com/finance/info?infotype=infoquoteall&q=" + stockSymbol)
-statusCode = response.code
-print statusCode
+# print response.read()
+file = open("GoogleData.txt","r")
+for line in file.readlines():
+	if line.find('''"e" :''') != -1:
+		exchangeName = (line.split('''"'''))[3]
+file.close()
 
-response = br.open("http://www.google.com/finance/info?infotype=infoquoteall&q=" + stockSymbol)
-print response.read()
+usExchanges = ['NASDAQ','NYSE','AMEX']
+canExchanges = ['TSE']
 
-response = br.open("http://www.stock2own.com/StockAnalysis/Stock/" + stockCountry + "/" + stockSymbol + "/")
-# for form in br.forms():
-# 	print form.name
+if exchangeName.upper() in usExchanges:
+	stockCountry = 'US'
+elif exchangeName.upper() in canExchanges:
+	stockCountry = 'CA'
+
+br.open("http://www.stock2own.com/StockAnalysis/Stock/" + stockCountry + "/" + stockSymbolInput + "/")
+soup = BeautifulSoup(br.response().read())
+print soup.prettify()[0:100]
+
+
+
+
 print br.title()
-# print br.forms()
+
